6feee735ea3e5cf525ef220f7603a05bb05a97f0
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-29 19:17:46 -0300

    edita migration Users com as colunas necessarias

--- backend/migrations/20211129221057-create-users.js
+++ backend/migrations/20211129221057-create-users.js
@@ -2,20 +2,34 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      image: {
+        allowNull: true,
+        type: Sequelize.STRING,
+        field: 'image',
+      } 
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    return queryInterface.dropTable('Users');
+  },
 };
