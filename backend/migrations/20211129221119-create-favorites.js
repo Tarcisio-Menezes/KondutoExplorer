0b21d9d5cfa5e83743bbb6fa8b3df06e6cc6f7a6
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-11-29 19:26:11 -0300

    edita migration Favorites com as colunas necessarias

--- backend/migrations/20211129221119-create-favorites.js
+++ backend/migrations/20211129221119-create-favorites.js
@@ -2,20 +2,37 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    return queryInterface.createTable('Favorites', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      imagePath: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      rover: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      camera: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      landing: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+      launch: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      } 
+    })
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: async (queryInterface, _Sequelize) => {
+    return queryInterface.dropTable('Favorites');
+  },
 };
