d0ec2f46dbf7fb3f67c4a56c3c82519a8ca63dad
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-12-03 12:15:47 -0300

    adiciona cors

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const usersRoutes = require('../routes/usersRoute');
 const usersMiddleware = require('../middlewares/usersMiddleware');
 const favoriteRoutes = require('../routes/favoritesRoute');
@@ -9,6 +10,9 @@ const loginMiddleware = require('../middlewares/loginMiddleware');
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use(usersRoutes);
 app.use(usersMiddleware);
