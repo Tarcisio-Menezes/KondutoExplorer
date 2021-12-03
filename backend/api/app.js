e290db3ef2939c5a5d97d403fd10f47e2e16251e
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-12-03 11:32:02 -0300

    adiciona login em app

--- backend/api/app.js
+++ backend/api/app.js
@@ -3,6 +3,8 @@ const usersRoutes = require('../routes/usersRoute');
 const usersMiddleware = require('../middlewares/usersMiddleware');
 const favoriteRoutes = require('../routes/favoritesRoute');
 const favoriteMiddleware = require('../middlewares/favoriteMiddleware');
+const loginRoutes = require('../routes/loginRoute');
+const loginMiddleware = require('../middlewares/loginMiddleware');
 
 const app = express();
 
@@ -13,5 +15,8 @@ app.use(usersMiddleware);
 
 app.use(favoriteRoutes);
 app.use(favoriteMiddleware);
+
+app.use(loginRoutes);
+app.use(loginMiddleware);
 
 module.exports = app;
