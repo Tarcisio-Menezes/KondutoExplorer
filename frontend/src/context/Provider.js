11b160d5fa2a0eca0e3062c36005ddb9e6227aa3
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-12-03 12:39:53 -0300

    adiciona context e provider

---
+++ frontend/src/context/Provider.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import MainContext from './MainContext';
+
+const Provider = ({ children }) => {
+  const [userName, setUserName] = useState('');
+  const [userPass, setUserPass] = useState('');
+  const [registerUser, setRegisterUser] = useState('');
+  const [token, setToken] = useState('');
+  const [favorites, setFavorites] = useState('');
+
+  const context = {
+    userName,
+    setUserName,
+    userPass,
+    setUserPass,
+    token,
+    setToken,
+    registerUser,
+    setRegisterUser,
+    favorites,
+    setFavorites,
+  };
+
+  return (
+    <MainContext.Provider value={ context }>
+      {children}
+    </MainContext.Provider>
+  );
+};
+
+export default Provider;
+
+Provider.propTypes = {
+  children: PropTypes.array,
+}.isRequired;
