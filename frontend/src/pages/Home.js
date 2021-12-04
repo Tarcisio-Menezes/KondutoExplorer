3529be73715cefb8c9fffbab54e867ab52a33e0d
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-12-04 12:10:33 -0300

    adiciona navegaçao por paginas

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,16 @@
 /* eslint-disable react/void-dom-elements-no-children */
 import React, { useState, useEffect } from 'react';
-// import { Card } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 // import axios from 'axios';
 // import MainContext from '../context/MainContext';
-import { getAllDayOneRover } from '../utils/requestAPI';
+import { getDatesAndPhotos } from '../utils/requestAPI';
 
 function Home() {
   // const { userName, token, setFavorites } = useContext(MainContext);
   const [rover, setRover] = useState('');
   const [dataRover, setDataRover] = useState('');
+  const [day, setDay] = useState(1);
+  const [page, setPage] = useState(1);
 
   // const headers = {
   //   'Content-Type': 'application/json',
@@ -17,14 +19,16 @@ function Home() {
   // };
 
   useEffect(() => {
-    async function getAllRoversInDayOne() {
+    async function getAllDataInSelectedDay() {
       if (rover) {
-        const data = await getAllDayOneRover(rover);
+        const data = await getDatesAndPhotos(rover, day, page);
+        console.log(data);
+        console.log(rover);
         return setDataRover(data);
       }
     }
-    getAllRoversInDayOne();
-  }, [rover]);
+    getAllDataInSelectedDay();
+  }, [day, page, rover]);
 
   const selectRover = () => (
     <section>
@@ -69,8 +73,8 @@ function Home() {
   );
 
   const infosSelectedRover = () => {
-    const { photos } = dataRover;
     if (dataRover) {
+      const { photos } = dataRover;
       const imagesIndDayOne = photos.length;
       const maxPage = 25;
       return (
@@ -89,11 +93,45 @@ function Home() {
     } return null;
   };
 
+  const viewDatesAndPhotos = () => {
+    if (dataRover) {
+      return (
+        <div>
+          <h3>Konduter, agora vamos explorar ainda mais</h3>
+          <p>
+            Utilize os campos abaixo para fazer uma pesquisa combinada.
+            Digite um número inteiro que representará o dia em marte em
+            que deseja ver algumas imagens, contando a partir da data
+            em que o rover chegou. Dica: 1 representa o
+            primeiro dia do rover no planeta, 2 representa o segundo e assim por diante.
+          </p>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Dia marciano</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Digite o dia."
+              onChange={ ({ target }) => setDay(target.value) }
+            />
+            <Form.Label>Navegue pelas páginas</Form.Label>
+            <Form.Control
+              type="number"
+              min="0"
+              step="1"
+              placeholder="Página."
+              onChange={ ({ target }) => setPage(target.value) }
+            />
+          </Form.Group>
+        </div>
+      );
+    }
+  };
+
   return (
     <div>
       Hello home
       { selectRover() }
       { infosSelectedRover() }
+      { viewDatesAndPhotos() }
     </div>
   );
 }
