73dc41d94fad71d6cd0b0c1fdcd35262748a4fa8
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-12-05 15:56:11 -0300

    ajusta mensagem de erro em middleware complementar

--- backend/middlewares/usersMiddlewareName.js
+++ backend/middlewares/usersMiddlewareName.js
@@ -1,6 +1,6 @@
 module.exports = (err, _req, res, _next) => {
   if (err.code === 'invalidName') {
-    return res.status(400).json({ message: '"password" length must be 6 characters long' });
+    return res.status(400).json({ message: '"name" length must be at least 8 characters long' });
   }
 
   return res.status(500).json({ message: 'Critcal Error :(' });
